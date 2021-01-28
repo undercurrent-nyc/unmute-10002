8cb07b795eb3dc2e157c74cbcf0d623f898f8473
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2021-01-28 08:54:01 -0500

    Make a rando route

--- app/router.js
+++ app/router.js
@@ -8,5 +8,6 @@ export default class Router extends EmberRouter {
 
 Router.map(function () {
   this.route("coming-soon");
-  this.route('welcome-screen');
+  this.route("welcome-screen");
+  this.route('rando');
 });
---
+++ app/routes/rando.js
@@ -0,0 +1,4 @@
+import Route from '@ember/routing/route';
+
+export default class RandoRoute extends Route {
+}
