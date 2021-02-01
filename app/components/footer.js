918633c1870ed574b7d4b81c9552bbfbabb2d01d
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2021-02-01 01:42:47 -0500

    feat: change logos to 4 x 4 grid.

--- app/components/footer.js
+++ app/components/footer.js
@@ -49,17 +49,17 @@ export default class FooterComponent extends Component {
         name: "wallonia"
       }
     ]);
-    // Austria has to be one of the first 5 logos.
-    shuffledCountries.splice(Math.floor(Math.random() * 5), 0, {
+    // Austria has to be one of the first 8 logos.
+    shuffledCountries.splice(Math.floor(Math.random() * 8), 0, {
       website: "https://www.acfny.org/",
       name: "austria",
       extraClass: "row-span-2",
     });
-    // EUNIC has to be index 9.
-    shuffledCountries.splice(9, 0, {
+    // EUNIC has to be index 11.
+    shuffledCountries.splice(11, 0, {
       website: "https://eunicglobal.eu",
       name: "eunic-eu",
-      extraClass: "col-span-2",
+      extraClass: "col-span-3",
     });
 
     return shuffledCountries;
