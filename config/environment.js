238ff6952f96f84ce16a83368fd9133cceb2c3e9
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2021-02-02 23:15:42 -0500

    deps: add markdown support

--- config/environment.js
+++ config/environment.js
@@ -6,6 +6,11 @@ module.exports = function (environment) {
     environment,
     rootURL: "/",
     locationType: "auto",
+    "ember-cli-markdown-resolver": {
+      folders: {
+        docs: "app/markdown"
+      },
+    },
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
