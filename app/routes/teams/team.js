7176179e91e8aa7740844d87f7ccca0f9100f7fe
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2021-04-03 04:19:17 -0400

    Include sneak peek in team videos.

--- app/routes/teams/team.js
+++ app/routes/teams/team.js
@@ -8,9 +8,18 @@ export default class TeamsTeamRoute extends Route {
     // Need to load all teams so relationship filter below works.
     const teams = await this.store.findAll("team", {include: "artists"});
     const team = await this.store.findRecord("team", team_id, { include: "artists" });
+    let sneakPeekId = "1oleCafkMhI";
+    const secondSneakPeekTeams = "key ram branch stream cloud".split(" ");
+    if (secondSneakPeekTeams.filter(team => team === team_id).length > 0) {
+      sneakPeekId = "1msh-bhFaLg";
+    }
+
+    const sneakPeekClip = clips.toArray().filter(e => e.id === sneakPeekId)[0];
+    sneakPeekClip.team = team;
+
     return RSVP.hash({
       team,
-      clips: clips.toArray().filter(e => e.team.get("id") === team.get("id")).sort((a, b) => (a.color > b.color) ? 1 : -1).reverse(),
+      clips: clips.toArray().filter(e => (e.team.get("id") === team.get("id") || e.id === sneakPeekId)).sort((a, b) => (a.color > b.color) ? 1 : -1).reverse(),
 
     });
   }
