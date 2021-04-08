b7fc2e33275c20373fbe7ad2092fa936c85cb450
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2021-04-08 16:38:59 -0400

    get sticky-sidebar working.

--- app/components/sticky-sidebar.js
+++ app/components/sticky-sidebar.js
@@ -1,9 +1,13 @@
 import Component from '@glimmer/component';
+import { action } from "@ember/object";
 import StickySidebar from "sticky-sidebar";
 
 export default class StickySidebarComponent extends Component {
-  constructor() {
-    super(...arguments);
-    const sidebar = new StickySidebar(".sticky-sidebar");
+  @action makeSticky() {
+    const sidebar = new StickySidebar("aside.sticky-sidebar", {
+      topSpacing: 210,
+      bottomSpacing: 300,
+      containerSelector: ".sticky-sidebar-container",
+    });
   }
 }
