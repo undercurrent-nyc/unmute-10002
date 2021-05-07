7dce2b5c1bfba5d7163f425cf0f0d8058aa7dc18
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2021-05-07 17:20:31 -0400

    Add logic to handle more than 100 clips.

--- app/adapters/clip.js
+++ app/adapters/clip.js
@@ -10,4 +10,31 @@ export default class ClipAdapter extends RESTAdapter {
     "Accept": "application/json",
     "Authorization": `Bearer ${ENV.AIRTABLE_API_KEY}`,
   }
+
+  findAll(store, type, sinceToken, snapshotRecordArray) {
+    let query = this.buildQuery(snapshotRecordArray);
+    let url = this.buildURL(type.modelName, null, snapshotRecordArray, 'findAll');
+    if (sinceToken) {
+      query.since = sinceToken;
+    }
+
+    return this.getClips(url, query);
+  }
+
+  async getClips(url, query){
+    const data = {
+      records: []
+    };
+    const response = await this.ajax(url, "GET", { data: query });
+    data.records = response.records;
+    if(response.offset){
+      const secondResponse = await this.ajax(`${url}?offset=${response.offset}`, "GET", { data: query });
+      for(const record of secondResponse.records){
+        data.records.push(record);
+      }
+    }
+    return data;
+  }
+
+
 }
